974052272272cdd035ba0fd105c282a74df9acf1
Rishabh Kumar <119288381+AlieenBotrishabh@users.noreply.github.com>
2025-02-13 00:26:38 +0530

    about us name

--- client/item/components/HomePage.jsx
+++ client/item/components/HomePage.jsx
@@ -23,7 +23,16 @@ const HomePage = () => {
   <h1 className='text-5xl font-bold text-white'>About Us</h1>
   </div>
   <div className='w-full h-[1000px] p-4'>
-    <div className='w-full h-2/4 border border-gray-600 rounded-lg'></div>
+    <div className='w-full h-2/4 border border-gray-600 rounded-lg flex flex-row items-center p-4'>
+    <div className='w-2/4 h-[400px] border border-gray-600 rounded-lg'>
+      <img src="" alt="image" />
+    </div>
+    <div className='w-2/4 h-[400px] flex flex-col items-center p-4'>
+      <h1 className='text-5xl font-bold text-white p-2'>Rishabh Kumar</h1>
+      <h2 className='text-xl text-green-500 p-2'>A CS student in India</h2>
+      <p className='text-xl text-gray-700 p-2'>Hi devs, My name is Rishabh Kumar. I am passionate CS undergrad who is keen to develop and solve problem through my projects</p>
+    </div>
+    </div>
   </div>
 </div>
 
